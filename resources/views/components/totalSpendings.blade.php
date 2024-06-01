f4a0fb5ddc3d4a8f74357eff0ef752a32f0dfb50
coremrazMac <reason1337@mail.ru>
2024-06-01 19:25:40 +0300

    totalsum design changed

--- resources/views/components/totalSpendings.blade.php
+++ resources/views/components/totalSpendings.blade.php
@@ -1,6 +1,5 @@
 @props(['total' => '1337'])
 
-<div class="flex flex-col items-center w-1/12 bg-gray-800 rounded-xl m-4">
-
+<div class="flex flex-col items-center w-1/6 bg-gray-800 rounded-xl m-4 p-4">
     <h1 class="text-amber-50 font-bold">Total sum: {{$total . "₽"}}</h1>
 </div>
